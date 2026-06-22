d4b1fa675795d679f7a1ab68b4ec3807d1590d4c
M-Saim-Khan <saim.khan9254@gmail.com>
2026-06-22 11:25:49 +0500

    fixed the database issue

--- store/settings.py
+++ store/settings.py
@@ -100,10 +100,10 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
